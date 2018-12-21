a3b0f8923ca06fe03064928ff0ce7045fcb10ed4
learn <eric.desimone@cortland.edu>
2018-12-21 15:56:23 -0500

    updated

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,19 @@
 module UserHelper
+
+  def image
+    if @user.avatar.url != "/avatars/original/missing.png"
+      image_tag @user.avatar.url(:medium)
+    else
+      "NO IMAGE AVAILABLE"
+    end
+  end
+
+  def profile_head
+    if current_user == @user
+      "Welcome, #{@user.first_name}!"
+    else
+      "Welcome to #{@user.first_name}'s profile!"
+    end
+  end
+
 end
